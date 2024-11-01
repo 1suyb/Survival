025c287c36af84de6ee97bb108ca850f82a12f61
udiladido <89720389+udiladido@users.noreply.github.com>
2024-11-01 22:28:12 +0900

    Update : Excel 파일 연동 스크립트 작성
    Excel 파일 유니티 Project 추가
    자세한 사용 방법 노션 페이지에 정리
    
    BuildItemData 스크립트 작성

---
+++ Survival/Assets/Scripts/DB/BuildItemDB.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BuildItemDB : Singleton<BuildItemDB> 
+{
+
+    //엑셀 데이터 스크립트 
+    private Dictionary<int, BuildItemData_BuildItem> _itmes = new ();
+
+
+    private void Start()
+    {
+        ItemDB();
+    }
+
+
+    public void ItemDB()
+    {
+            
+        //엑셀 파일 ReImport시 생성된 Asset 스크립트 Load, 주소에 스크립트 이름 넣기 
+        var res = Resources.Load<BuildItemData>("DataSO/BuildItemData");
+        var itemSO = Object.Instantiate(res);
+
+        //BuildItemData에 List 이름
+        var entities = itemSO.BuildItemList;
+
+        if (entities == null || entities.Count <= 0)
+            return;
+        
+        var entitityCount = entities.Count;
+        for (int i = 0; i < entitityCount; i++)
+        { 
+        
+            var item = entities[i];
+
+            if (_itmes.ContainsKey(item.Id))
+                _itmes[item.Id] = item;
+            else
+                _itmes.Add(item.Id, item);
+           
+        }
+       
+    }
+
+
+    //엑셀 데이터 스크립트 
+    public BuildItemData_BuildItem Get(int id)
+    {
+
+        if(_itmes.ContainsKey(id))
+            return _itmes[id];
+
+        return null;
+    }
+
+
+    public IEnumerator DbEnumerator()
+    {
+
+        return _itmes.GetEnumerator();
+    }
+
+}

ad92b194f4a0e7e16ddfa6b8f468d573e5324a73
OTH <112844098+LDOTH1015@users.noreply.github.com>
2024-11-05 23:52:33 +0900

    Fix : 플레이어 데이터 생성 및 코드 수정

--- Survival/Assets/Scripts/Player/Player.cs
+++ Survival/Assets/Scripts/Player/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
+    public PlayerData data;
     private void Awake()
     {
         PlayerManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
+        data = GetComponent<PlayerData>();
     }
 }
